c5d6c58fea896707fa2993bbf6c7a9dd6465b3cd
Rezza Ilham <rezzailham300800@gmail.com>
2025-08-28 12:46:25 +0700

    Adding Register User Test Suites And TestListener

---
+++ Test Suites/RegisterUserTestSuites.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegisterUserTestSuites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>20b56e27-53e2-4ca9-9618-dca15786a53f</testSuiteGuid>
+   <testCaseLink>
+      <guid>662b044e-4287-4ff9-a909-6bd5a5078155</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/registerUserTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
